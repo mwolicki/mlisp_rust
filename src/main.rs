c4ad4bf9c9f3be835f1c3e40cd731befbbc117e3
Marcin Wolicki <marcin.wolicki@gmail.com>
2017-10-06 15:50:48 +0100

    any

--- src/main.rs
+++ src/main.rs
@@ -12,31 +12,29 @@ enum ParseResult<'a, T> {
     Fail(&'a str, &'a [char]),
 }
 
-struct AndParser<'a, A, ARet, B, BRet>
+#[derive(Clone, Copy)]
+struct BothParser<'a, A, B>
 where
-    A: Parser<'a, ARet> + 'a,
-    B: Parser<'a, BRet> + 'a,
+    A: Parser<'a>,
+    B: Parser<'a>,
 {
-    c: PhantomData<(ARet, BRet)>,
-    a: &'a A,
-    b: &'a B,
+    left: A,
+    right: B,
+    phantom: PhantomData<&'a i8>,
 }
 
-impl<'a, A, ARet, B, BRet> Parser<'a, (ARet, BRet)> for AndParser<'a, A, ARet, B, BRet>
+impl<'a, A, ARet, B, BRet> Parser<'a> for BothParser<'a, A, B>
 where
-    A: Parser<
-        'a,
-        ARet,
-    >,
-    B: Parser<
-        'a,
-        BRet,
-    >,
+    A: Parser<'a, Return = ARet>,
+    A: Copy,
+    B: Parser<'a, Return = BRet>,
+    B: Copy,
 {
-    fn parse(&self, txt: &'a [char]) -> ParseResult<'a, (ARet, BRet)> {
-        match self.a.parse(txt) {
+    type Return = (ARet, BRet);
+    fn parse(&self, txt: &'a [char]) -> ParseResult<'a, Self::Return> {
+        match self.left.parse(txt) {
             ParseResult::Ok(a_corr) => {
-                match self.b.parse(a_corr.txt) {
+                match self.right.parse(a_corr.txt) {
                     ParseResult::Ok(b_corr) => ParseResult::Ok(Corr {
                         txt: b_corr.txt,
                         res: (a_corr.res, b_corr.res),
@@ -49,25 +47,174 @@ where
     }
 }
 
-trait Parser<'a, Return> {
-    fn parse(&self, txt: &'a [char]) -> ParseResult<'a, Return>;
+
+#[derive(Clone, Copy)]
+struct LeftParser<'a, A, B>
+where
+    A: Parser<'a>,
+    B: Parser<'a>,
+{
+    left: A,
+    right: B,
+    phantom: PhantomData<&'a i8>,
 }
 
-fn and<'a, A, ARet, B, BRet>(a: &'a A, b: &'a B) -> AndParser<'a, A, ARet, B, BRet>
+impl<'a, A, ARet, B> Parser<'a> for LeftParser<'a, A, B>
 where
-    A: Parser<'a, ARet> + 'a,
-    B: Parser<'a, BRet> + 'a,
+    A: Parser<'a, Return = ARet>,
+    A: Copy,
+    B: Parser<'a>,
+    B: Copy,
 {
-    AndParser {
-        a,
-        b,
-        c: PhantomData,
+    type Return = ARet;
+    fn parse(&self, txt: &'a [char]) -> ParseResult<'a, Self::Return> {
+        match self.left.parse(txt) {
+            ParseResult::Ok(a_corr) => {
+                match self.right.parse(a_corr.txt) {
+                    ParseResult::Ok(b_corr) => ParseResult::Ok(Corr {
+                        txt: b_corr.txt,
+                        res: a_corr.res,
+                    }),
+                    ParseResult::Fail(res, txt) => ParseResult::Fail(res, txt),
+                }
+            }
+            ParseResult::Fail(res, txt) => ParseResult::Fail(res, txt),
+        }
     }
 }
 
+
+
+#[derive(Clone, Copy)]
+struct RightParser<'a, A, B>
+where
+    A: Parser<'a>,
+    B: Parser<'a>,
+{
+    left: A,
+    right: B,
+    phantom: PhantomData<&'a i8>,
+}
+
+impl<'a, A, B, RetB> Parser<'a> for RightParser<'a, A, B>
+where
+    A: Parser<'a>,
+    A: Copy,
+    B: Parser<'a, Return = RetB>,
+    B: Copy,
+{
+    type Return = RetB;
+    fn parse(&self, txt: &'a [char]) -> ParseResult<'a, Self::Return> {
+        match self.left.parse(txt) {
+            ParseResult::Ok(a_corr) => {
+                match self.right.parse(a_corr.txt) {
+                    ParseResult::Ok(b_corr) => ParseResult::Ok(Corr {
+                        txt: b_corr.txt,
+                        res: b_corr.res,
+                    }),
+                    ParseResult::Fail(res, txt) => ParseResult::Fail(res, txt),
+                }
+            }
+            ParseResult::Fail(res, txt) => ParseResult::Fail(res, txt),
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+struct AllParser<'a, P>
+where
+    P: Parser<'a> + 'a,
+{
+    parser: P,
+    phantom: PhantomData<&'a PhantomData<P>>,
+}
+
+
+impl<'a, P, Ret> Parser<'a> for AllParser<'a, P>
+where
+    P: Parser<'a, Return = Ret>,
+    Ret: 'a,
+    P: Copy,
+{
+    type Return = Vec<Ret>;
+    fn parse(&self, txt: &'a [char]) -> ParseResult<'a, Self::Return> {
+        let mut res = Vec::new();
+        let mut txt = txt;
+        loop {
+            if let ParseResult::Ok(corr) = self.parser.parse(txt) {
+                res.push(corr.res);
+                txt = corr.txt;
+            } else {
+                break;
+            }
+        }
+
+        if res.len() > 0 {
+            ParseResult::Ok(Corr { res, txt })
+        } else {
+            ParseResult::Fail("no matches", txt)
+        }
+    }
+}
+
+
+trait Parser<'a>
+where
+    Self: std::marker::Sized,
+    Self: Copy,
+{
+    type Return;
+    fn parse(&self, txt: &'a [char]) -> ParseResult<'a, Self::Return>;
+
+    fn both<B, BRet>(self, right: B) -> BothParser<'a, Self, B>
+    where
+        B: Parser<'a, Return = BRet>,
+        Self: std::marker::Sized,
+    {
+        BothParser {
+            left: self,
+            right,
+            phantom: PhantomData,
+        }
+    }
+
+    fn left<B, BRet>(self, right: B) -> LeftParser<'a, Self, B>
+    where
+        B: Parser<'a, Return = BRet>,
+        Self: std::marker::Sized,
+    {
+        LeftParser {
+            left: self,
+            right,
+            phantom: PhantomData,
+        }
+    }
+
+    fn right<B, BRet>(self, right: B) -> RightParser<'a, Self, B>
+    where
+        B: Parser<'a, Return = BRet>,
+        Self: std::marker::Sized,
+    {
+        RightParser {
+            left: self,
+            right,
+            phantom: PhantomData,
+        }
+    }
+
+    fn all(self) -> AllParser<'a, Self> {
+        AllParser {
+            parser: self,
+            phantom: PhantomData,
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
 struct CharParser(char);
 
-impl<'a> Parser<'a, char> for CharParser {
+impl<'a> Parser<'a> for CharParser {
+    type Return = char;
     fn parse(&self, txt: &'a [char]) -> ParseResult<'a, char> {
         if txt.len() > 0 && txt[0] == self.0 {
             ParseResult::Ok(Corr {
@@ -85,15 +232,17 @@ fn parse_char<'a>(ch: char) -> CharParser {
 }
 
 
-
 fn main() {
-    let x = "abcd";
+    let x = "abcddefg";
     let c: Vec<char> = x.chars().collect();
-    let a = parse_char('a');
-    let b = parse_char('b');
 
-    let p = and(&a, &b);
 
-    println!("Hello, world! {:?}", p.parse(&c));
-    println!("Hello, world! {:?}", p.parse(&c));
+    let p = parse_char('a').both(parse_char('b')).left(parse_char('c'));
+
+
+
+    println!("->: {:?}", p.parse(&c));
+    println!("->: {:?}", p.right(parse_char('d')).parse(&c));
+    println!("->: {:?}", p.right(parse_char('d').all()).parse(&c));
+    println!("->: {:?}", parse_char('d').parse(&c));
 }
