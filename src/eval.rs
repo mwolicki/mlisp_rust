cae6b1d876f2786fbe119934f7f17cde80cee0b6
Marcin Wolicki <marcin.wolicki@gmail.com>
2017-10-15 03:39:28 +0200

    renamed env var

--- src/eval.rs
+++ src/eval.rs
@@ -24,7 +24,7 @@ pub enum Value {
 //fix signature to use &str
 pub fn eval<'a, 'b>(expr: &'b Expr) -> Result<Value, &'a str> {
 
-    fn eval<'a, 'b>(expr: &'b Expr, values: Env) -> Result<Value, &'a str> {
+    fn eval<'a, 'b>(expr: &'b Expr, env: Env) -> Result<Value, &'a str> {
         match *expr {
             Expr::EInt(val) => Ok(Value::Atom(Atom::Int(val))),
             Expr::EStr(ref val) => Ok(Value::Atom(Atom::String(val.clone()))),
@@ -38,7 +38,7 @@ pub fn eval<'a, 'b>(expr: &'b Expr) -> Result<Value, &'a str> {
             }
             Expr::EList(ref name, ref vals) => {
                 let vals = vals.iter()
-                    .map(|v| eval(&v, values.clone()))
+                    .map(|v| eval(&v, env.clone()))
                     .collect::<Result<Vec<_>, _>>()?;
 
                 fn i64_calc<'a, F>(f: F, zero: i64, vals: &Vec<Value>) -> Result<Value, &'a str>
